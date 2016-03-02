fe072b62fef451da303d9b29e84c87d03d038abf
Fikri Rasyid <fikrirasyid@gmail.com>
2016-03-02 14:21:50 +0700

    Loaded Divi 100 after all Divi 100 plugins have been registered

--- article-card.php
+++ article-card.php
@@ -13,10 +13,80 @@
 * License: GPL3
 */
 
+
 /**
- * Load Divi 100 Setup
+ * Register plugin to Divi 100 list
  */
-require_once( plugin_dir_path( __FILE__ ) . 'divi-100-setup/divi-100-setup.php' );
+class ET_Divi_100_Article_Card_Config {
+	public static $instance;
+
+	/**
+	 * Hook the plugin info into Divi 100 list
+	 */
+	function __construct() {
+		add_filter( 'et_divi_100_settings', array( $this, 'register' ) );
+		add_action( 'plugins_loaded',       array( $this, 'init' ) );
+	}
+
+	/**
+	* Gets the instance of the plugin
+	*/
+	public static function instance(){
+		if ( null === self::$instance ){
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Define plugin info
+	 *
+	 * @return array plugin info
+	 */
+	public static function info() {
+		$main_prefix = 'et_divi_100_';
+		$plugin_slug = 'article_cards';
+
+		return array(
+			'main_prefix'        => $main_prefix,
+			'plugin_name'        => __( 'Article Card' ),
+			'plugin_description' => __( 'Nullam quis risus eget urna mollis ornare vel eu leo.' ),
+			'plugin_slug'        => $plugin_slug,
+			'plugin_id'          => "{$main_prefix}{$plugin_slug}",
+			'plugin_prefix'      => "{$main_prefix}{$plugin_slug}-",
+			'plugin_version'     => 20160301,
+			'plugin_dir_path'    => plugin_dir_path( __FILE__ ),
+		);
+	}
+
+	/**
+	 * et_divi_100_settings callback
+	 *
+	 * @param array  settings
+	 * @return array settings
+	 */
+	function register( $settings ) {
+		$info = self::info();
+
+		$settings[ $info['plugin_slug'] ] = $info;
+
+		return $settings;
+	}
+
+	/**
+	 * Init plugin after all plugins has been loaded
+	 */
+	function init() {
+		// Load Divi 100 Setup
+		require_once( plugin_dir_path( __FILE__ ) . 'divi-100-setup/divi-100-setup.php' );
+
+		// Load Article Card
+		ET_Divi_100_Article_Card::instance();
+	}
+}
+ET_Divi_100_Article_Card_Config::instance();
+
 
 /**
  * Load Article Cards
@@ -26,10 +96,7 @@ class ET_Divi_100_Article_Card {
 	 * Unique instance of plugin
 	 */
 	public static $instance;
-	public $main_prefix;
-	public $plugin_slug;
-	public $plugin_id;
-	public $plugin_prefix;
+	public $config;
 
 	/**
 	 * Gets the instance of the plugin
@@ -46,10 +113,7 @@ class ET_Divi_100_Article_Card {
 	 * Constructor
 	 */
 	private function __construct(){
-		$this->main_prefix   = 'et_divi_100_';
-		$this->plugin_slug   = 'article_card';
-		$this->plugin_prefix = "{$this->main_prefix}{$this->plugin_slug}-";
-		$this->plugin_id     = "{$this->main_prefix}{$this->plugin_slug}";
+		$this->config = ET_Divi_100_Article_Card_Config::info();
 
 		// Initialize if Divi is active
 		if ( et_divi_100_is_active() ) {
@@ -67,9 +131,9 @@ class ET_Divi_100_Article_Card {
 
 		if ( is_admin() ) {
 			$settings_args = array(
-				'plugin_id'   => $this->plugin_id,
-				'title'       => __( 'Article Cards' ),
-				'description' => __( 'Nullam quis risus eget urna mollis ornare vel eu leo.' ),
+				'plugin_id'   => $this->config['plugin_id'],
+				'title'       => $this->config['plugin_name'],
+				'description' => $this->config['plugin_description'],
 				'fields'      => array(),
 			);
 
@@ -82,8 +146,7 @@ class ET_Divi_100_Article_Card {
 	 * @return void
 	 */
 	function enqueue_frontend_scripts() {
-		wp_enqueue_style( 'custom-article-cards', plugin_dir_url( __FILE__ ) . 'assets/css/style.css' );
-		wp_enqueue_script( 'custom-article-cards', plugin_dir_url( __FILE__ ) . 'assets/js/scripts.js', array( 'jquery', 'divi-custom-script' ), '0.0.1', true );
+		wp_enqueue_style( 'custom-article-cards', plugin_dir_url( __FILE__ ) . 'assets/css/style.css', array(), $this->config['plugin_version'] );
+		wp_enqueue_script( 'custom-article-cards', plugin_dir_url( __FILE__ ) . 'assets/js/scripts.js', array( 'jquery', 'divi-custom-script' ), $this->config['plugin_version'], true );
 	}
 }
-ET_Divi_100_Article_Card::instance();
